41c2429eb2ef005fae4f30839c4b74bdef8c7a4a
Keagan Gradidge <gradidgek@yahoo.com>
2018-11-28 00:55:12 +1300

    Added optional default header override to all public methods

--- KGSoft.TinyHttpClient/Config/HttpClientPool.cs
+++ KGSoft.TinyHttpClient/Config/HttpClientPool.cs
@@ -14,28 +14,9 @@ namespace KGSoft.TinyHttpClient.Config
                 if (_httpClient == null)
                 {
                     _httpClient = new HttpClient();
-
-                    if (HttpConfig.DefaultAuthHeader != null)
-                        _httpClient.DefaultRequestHeaders.Authorization = HttpConfig.DefaultAuthHeader;
-
-                    if (HttpConfig.CustomHeaders != null)
-                        foreach (var kvp in HttpConfig.CustomHeaders)
-                            _httpClient.DefaultRequestHeaders.Add(kvp.Key, kvp.Value);
-
-                    _httpClient.DefaultRequestHeaders.Accept.Add(
-                        new MediaTypeWithQualityHeaderValue(HttpConfig.MediaTypeHeader));
                 }
                 return _httpClient;
             }
-        }
-
-        /// <summary>
-        /// Since we're re-using a single HttpClient, this method allows us to reset our HttpClient in case we need to change any config
-        /// after HttpClient has been instatiated for the first time
-        /// </summary>
-        public static void Reset()
-        {
-            _httpClient = null;
         }
     }
 }
